48b08f0cb4b63f964b4ed6086b5c79642076f8ae
zianejaz <zianejaz@gmail.com>
2022-05-19 16:18:51 +0500

    added user json < json not mapping from state

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react'
+import UserBlock from './userBlock'
+
+export class Users extends Component {
+    state = {
+        json: []
+    }
 
 
-const Users = ()=>{
-    return(
-        <div>
-
-            <h1>Users content</h1>
-
-        </div>
-    )
+    //fetch json data from jsonplaceholder api
+    jsonFetch = () => {
+        fetch('https://jsonplaceholder.typicode.com/users/')
+            .then(response => response.json())
+            .then(json => this.setState({json}))
 }
+    jsonToUserBlock = this.state.json.map((item, index)=>{
+        return (
+            console.log(this.state.json.length, index)
+        )
+    })
+
+    componentDidMount() {
+        this.jsonFetch()
+    }
+
+    render() {
+        return (
+            <div>users
+                < UserBlock />
+            </div>
+        )
+    }
+}
+
 export default Users
