5343ee5a874e4b5d73bd9c6bc5a1b16a37791e2b
Zian Ejaz <zianejaz@gmail.com>
2022-05-24 21:13:30 +0500

    used useState hook

--- src/components/hooks/hooksMain.js
+++ src/components/hooks/hooksMain.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Hooks = () => {
+  const [count, setCount] = useState(2)
+  const [login, isLogged] = useState()
   return (
-    <div>hooksMain</div>
+    <div className={`flex flex-wrap ${login === true ? "bg-green-500" : "bg-red-500"}`}>
+      <div className='border p-3 w-full'>
+        <button className='border p-3 shadow-xl w-full' onClick={() => setCount(count + 1)}>click me</button>
+        <p>{count}</p>
+      </div>
+
+      <div className='border p-3 w-full'>
+        <button className='border p-3 shadow-xl w-full' onMouseEnter={() => isLogged(true)} onMouseLeave={()=> isLogged(false)}>Hover me</button>
+        <p>{login}</p>
+      </div>
+
+
+    </div>
   )
 }
 
